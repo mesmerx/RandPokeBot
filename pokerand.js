8ee7883a7d07f81ebd0a69a12bc87fbc08f35aad
Enzzo Cavallo <souenzzo@gmail.com>
2018-01-22 21:22:56 -0200

    testing rollDice

--- pokerand.js
+++ pokerand.js
@@ -2,6 +2,16 @@ const math = require('mathjs')
 const fs = require('fs')
 const TelegramBot = require('node-telegram-bot-api')
 
+const mainKeyboard = {
+    reply_markup: {
+        keyboard: [
+            ['Attack', 'Defend'],
+            ['Use an Item', 'Roll a Dice'],
+            ['Restart'],
+        ]
+    }, parse_mode: 'HTML',
+}
+
 const IntRand = (x, y) => {
     const n = math.randomInt(x, y)
     return n
@@ -20,73 +30,64 @@ const checkPlayer = ({player1, player2, msg, bot}) => {
     }
 }
 
+const rollDice = ({match, player1, player2, bot, msg}) => {
+    let chatId = msg.chat.id
+    checkPlayer({player1, player2, bot, msg})
+    if (player1.id === msg.from.id && player1.num === undefined) {
+        player1.num = IntRand(1, 20)
+        bot.sendMessage(chatId, `Player 1 = ${player1.name} , <pre>You got: ${player1.num} </pre>`, {parse_mode: 'HTML'})
+        player1.life = 10
+        player1.item = 1
+        player1.turnAtk = 0
+        player1.turnDef = 0
+        console.log(`Player 1 = ${player1.name} , ID = ${player1.id} , Dice: ${player1.num}`)
+    } else if (player1.id === msg.from.id) {
+        bot.sendMessage(chatId, `${player1.name}, you cannot roll a dice again. Wait for another player!`)
+    } else {
+        if (player2.id === msg.from.id && player2.num === undefined) {
+            player2.num = IntRand(1, 20)
+            bot.sendMessage(chatId, `Player 2 = ${player2.name} , <pre>You got: ${player2.num} </pre>`, {parse_mode: 'HTML'})
+            player2.life = 10
+            player2.item = 1
+            player2.turnAtk = 0
+            player2.turnDef = 0
+            console.log(`Player 2 = ${player2.name} , ID = ${player2.id} , Dice: ${player2.num}`)
+        }
+    }
+    if (player1.num === player2.num && player1.id !== undefined) {
+        bot.sendMessage(chatId, 'Players got the same result. Please roll a dice again!')
+        player1.num = undefined
+        player1.turnAtk = 0
+        player1.turnDef = 0
+        player1.item = 1
+        player2.num = undefined
+        player2.turnAtk = 0
+        player2.turnDef = 0
+        player2.item = 1
+    } else if (player1.num > player2.num) {
+        bot.sendMessage(chatId, `${player1.name} will attack first!`)
+        player1.turnAtk = 1
+        match.value = 1
+    } else if (player2.num > player1.num) {
+        bot.sendMessage(chatId, `${player2.name} will attack first!`)
+        player2.turnAtk = 1
+        match.value = 1
+    }
+}
+
 const Player = {}
 
 //MAIN FUNCTION - START
 const main = ({token}) => {
     const bot = new TelegramBot(token, {polling: true})
-
-    let match
+    let match = {value: 0}
     let player1 = Object.create(Player)
     let player2 = Object.create(Player)
     bot.on('message', (msg) => {
         let chatId = msg.chat.id
-        const mainKeyboard = {
-            reply_markup: {
-                keyboard: [
-                    ['Attack', 'Defend'],
-                    ['Use an Item', 'Roll a Dice'],
-                    ['Restart'],
-                ]
-            }, parse_mode: 'HTML',
-        }
         // Functions Declaration - START //
-        const rollDice = () => {
-            checkPlayer({player1, player2, bot, msg})
-            if (player1.id === msg.from.id && player1.num === undefined) {
-                player1.num = IntRand(1, 20)
-                bot.sendMessage(chatId, `Player 1 = ${player1.name} , <pre>You got: ${player1.num} </pre>`, {parse_mode: 'HTML'})
-                player1.life = 10
-                player1.item = 1
-                player1.turnAtk = 0
-                player1.turnDef = 0
-                console.log(`Player 1 = ${player1.name} , ID = ${player1.id} , Dice: ${player1.num}`)
-            } else if (player1.id === msg.from.id) {
-                bot.sendMessage(chatId, `${player1.name}, you cannot roll a dice again. Wait for another player!`)
-            } else {
-                if (player2.id === msg.from.id && player2.num === undefined) {
-                    player2.num = IntRand(1, 20)
-                    bot.sendMessage(chatId, `Player 2 = ${player2.name} , <pre>You got: ${player2.num} </pre>`, {parse_mode: 'HTML'})
-                    player2.life = 10
-                    player2.item = 1
-                    player2.turnAtk = 0
-                    player2.turnDef = 0
-                    console.log(`Player 2 = ${player2.name} , ID = ${player2.id} , Dice: ${player2.num}`)
-                }
-            }
-            if (player1.num === player2.num && player1.id !== undefined) {
-                bot.sendMessage(chatId, 'Players got the same result. Please roll a dice again!')
-                player1.num = undefined
-                player1.turnAtk = 0
-                player1.turnDef = 0
-                player1.item = 1
-                player2.num = undefined
-                player2.turnAtk = 0
-                player2.turnDef = 0
-                player2.item = 1
-            } else if (player1.num > player2.num) {
-                bot.sendMessage(chatId, `${player1.name} will attack first!`)
-                player1.turnAtk = 1
-                match = 1
-            } else if (player2.num > player1.num) {
-                bot.sendMessage(chatId, `${player2.name} will attack first!`)
-                player2.turnAtk = 1
-                match = 1
-            }
-
-        }
         const restartGame = () => {
-            match = 0
+            match.value = 0
             player1.life = 10
             player1.id = undefined
             player1.item = 1
@@ -270,10 +271,10 @@ const main = ({token}) => {
         //Roll a dice
         let dice = 'Roll a Dice'
         if (msg.text.indexOf(dice) === 0) {
-            if (match === 1) {
+            if (match.value === 1) {
                 bot.sendMessage(chatId, `<b>Sorry, ${msg.from.first_name}!</b>\nYou cannot roll a dice while a match is in progress`, {parse_mode: 'html'})
             } else {
-                rollDice(player1, player2)
+                rollDice({player1, player2, match, msg, bot})
             }
         }
 
@@ -327,7 +328,7 @@ const main = ({token}) => {
                 } else {
                     bot.sendMessage(chatId, `${player1.name}, you've already used an item. You cannot use it twice, in the same match`)
                 }
-            } else if (msg.from.id === player1.id && player1.turnAtk <= 0 && match === 1) {
+            } else if (msg.from.id === player1.id && player1.turnAtk <= 0 && match.value === 1) {
                 bot.sendMessage(chatId, `${player1.name}, you cannot use an item right now. Wait your attack turn!`)
             }
 
@@ -338,7 +339,7 @@ const main = ({token}) => {
                 } else {
                     bot.sendMessage(chatId, `${player2.name}, you've already used an item. You cannot use it twice, in the same match`)
                 }
-            } else if (msg.from.id === player2.id && player2.turnAtk <= 0 && match === 1) {
+            } else if (msg.from.id === player2.id && player2.turnAtk <= 0 && match.value === 1) {
                 bot.sendMessage(chatId, `${player2.name}, you cannot use an item right now. Wait your attack turn!`)
             }
 
@@ -354,3 +355,4 @@ const main = ({token}) => {
 exports.main = main
 exports.checkPlayer = checkPlayer
 exports.Player = Player
+exports.rollDice = rollDice
--- pokerand.test.js
+++ pokerand.test.js
@@ -1,4 +1,4 @@
-const {checkPlayer, Player} = require('./pokerand')
+const {checkPlayer, Player, rollDice} = require('./pokerand')
 
 // const mock_msg = {"message_id":7205,"from":{"id":116632598,"is_bot":false,"first_name":"Enzzo","username":"souenzzo","language_code":"en"},"chat":{"id":-209063185,"title":"RandPokeBot - Project Lab","type":"group","all_members_are_administrators":true},"date":1516237642,"reply_to_message":{"message_id":7164,"from":{"id":458733904,"is_bot":true,"first_name":"PokeRand Games","username":"Bertinnnbot"},"chat":{"id":-209063185,"title":"RandPokeBot - Project Lab","type":"group","all_members_are_administrators":true},"date":1516237493,"text":"Hello, Enzzo!\nWelcome to PokeRand Game\nTap Roll a Dice to start","entities":[{"offset":6,"length":7,"type":"bold"}]},"text":"Attack"}
 
@@ -52,5 +52,21 @@ test('checkPlayer - terceiro jogador TENTA entrar',
             id: 123,
             msg: 'There are already two players in the game. Please, wait until the current match is over!'
         }])
+    }
+)
+
+test('rollDice',
+    () => {
+        const player1 = Object.create(Player)
+        const player2 = Object.create(Player)
+        const match = {value: 0}
+        const msg = {from: {id: 123, first_name: 'Tester'}, chat: {id: 123}}
+        let botState = []
+        const bot = {chat: {id: 0}, sendMessage: (id, msg) => botState.push({id, msg})}
+        rollDice({match, player1, player2, msg, bot})
+        const randNum = player1.num
+        expect(player1).toEqual({id: 123, item: 1, life: 10, name: 'Tester', num: randNum, turnAtk: 0, turnDef: 0})
+        expect(player2).toEqual({})
+        expect(botState).toEqual([{id: 123, msg: `Player 1 = Tester , <pre>You got: ${randNum} </pre>`}])
     }
 )
