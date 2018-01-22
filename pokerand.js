2e32128e88b2a53c1cfc8d01744691740096590f
Enzzo Cavallo <souenzzo@gmail.com>
2018-01-21 23:31:08 -0200

    Static functions

--- pokerand.js
+++ pokerand.js
@@ -2,160 +2,175 @@ const math = require("mathjs")
 const fs = require("fs")
 const TelegramBot = require("node-telegram-bot-api")
 
-exports.main = ({token}) => {
+const arrayDano = ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "3", "3", "3", "4", "4", "6", "0", "0", "0", "0", "0", "0", "0", "0", "5", "5", "7", "7"]
+const dictDano = {
+    0: ["Your opponent missed the attack!"],
+    1: ["It was effective! You lose 1 HP."],
+    2: ["It was effective! <b>CRITICAL HIT!</b> You lose 2 HP."],
+    3: ["It was ultra effective! You lose 3 HP."],
+    4: ["It was super effective! <b>CRITICAL HIT!</b> You lose 4 HP."],
+    5: ["It was effective! You lose 1 HP. You flinched! Opponent attacks again."],
+    6: ["It was ultra effective! <b>CRITICAL HIT!</b> You lose 6 HP."],
+    7: ["COUNTER! You take no damage and have an extra attack!"],
+}
+//Dict item
+const itemDict = {
+    0: ["You found nothing!"],
+    1: ["You found a Potion and recovered 1 HP!"],
+    2: ["You found a Super Potion and recovered 2 HP!"],
+    3: ["You found a Hyper Potion and recovered 3 HP!"],
+    4: ["You found a Flame Orb and lost 3 HP."],
+    5: ["You found a Quick Claw and can attack twice!"],
+}
+
+
+const mainKeyboard = {
+    reply_markup: {
+        keyboard: [
+            ["Attack", "Defend"],
+            ["Use an Item", "Roll a Dice"],
+            ["Restart"],
+        ]
+    }, parse_mode: "HTML",
+}
+
+const IntRand = (x, y) => math.randomInt(x, y)
+
+const checkPlayer = (msg, player1, player2, bot) => {
+    let chatId = msg.chat.id
+    if (player1.id === undefined) {
+        player1.id = msg.from.id
+        player1.name = msg.from.first_name
+        console.log(`Player 1 = ${player1.name} , ID = ${player1.id}`)
+    } else if (player2.id === undefined && player1.id !== msg.from.id) {
+        player2.id = msg.from.id
+        player2.name = msg.from.first_name
+        console.log(`Player 2 = ${player2.name} , ID = ${player2.id}`)
+    } else if ((msg.from.id !== player1.id) && (msg.from.id !== player2.id)) {
+        bot.sendMessage(chatId, "There are already two players in the game. Please, wait until the current match is over!")
+    }
+}
+
+const rollDice = (msg, player1, player2, bot) => {
+    let chatId = msg.chat.id
+    checkPlayer(msg, player1, player2, bot)
+    if (player1.id === msg.from.id) {
+        bot.sendMessage(chatId, `Player 1 = ${player1.name} , <pre>You got: ${IntRand(1, 21)} </pre>`, {parse_mode: "HTML"})
+    } else if (player2.id === msg.from.id) {
+        bot.sendMessage(chatId, `Player 2 = ${player2.name} , <pre>You got: ${IntRand(1, 21)} </pre>`, {parse_mode: "HTML"})
+    }
+}
+
+const restartGame = (msg, player1, player2, bot) => {
+    let chatId = msg.chat.id
+    player1.life = 10
+    player1.id = undefined
+    player1.item = 1
+    player2.life = 10
+    player2.id = undefined
+    player2.item = 1
+    bot.sendMessage(chatId, "Game restarted! Please ROLL A DICE.")
+}
+
+const writeRank = (player1, player2) => {
+    if (player1.life <= 0) {
+        fs.writeFile("rank.txt", `${player2.name}\n`, {flag: "a"}, function (err) {
+            if (err) throw err
+        })
+    } else if (player2.life <= 0) {
+        fs.writeFile("rank.txt", `${player1.name}\n`, {flag: "a"}, function (err) {
+            if (err) throw err
+        })
+    }
+}
+
+const randAttack = (msg, bot) => {
+    let chatId = msg.chat.id
+    fs.readFile("fileId.txt", function (err, data) {
+        if (err) throw err
+        let lines = data.toString().split("\n")
+        let randLine = lines[math.floor(math.random() * lines.length)]
+        bot.sendDocument(chatId, randLine)
+    })
+}
+
+const msgDef = (player, x, bot, msg) => {
+    let chatId = msg.chat.id
+    if (x === 5) {
+        player.dano = 1
+    }
+    else if (x === 7) {
+        player.dano = 0
+    }
+    else {
+        player.dano = x
+    }
+    player.life = player.life - player.dano
+    if (player.life <= 0) {
+        console.log(`${player.name} FAINTED`)
+        bot.sendMessage(chatId, `${player.name} <b>FAINTED!</b>\n<pre>Tap Restart.</pre>`, {parse_mode: "HTML"})
+    } else {
+        bot.sendMessage(chatId, ` ${dictDano[x]}\n${player.name} has ${player.life} HP!`, {parse_mode: "HTML"})
+    }
+}
+
+const defFunc = (player) => {
+    const n = IntRand(0, 37)
+    IntRand(0, 37, arrayDano[n])
+    let x = parseInt(arrayDano[n])
+    if (player.life === undefined) {
+        player.life = 10
+    }
+    console.log(`x = ${x}`)
+    msgDef(player, x)
+}
+
+const msgItem = (msg, bot, player, j) => {
+    let chatId = msg.chat.id
+    if (j === 4) {
+        player.life -= 3
+    }
+    else if (j === 0 || j === 5) {
+        player.life += 0
+    } else {
+        player.life += j
+    }
+    if (player.life <= 0) {
+        console.log(`${player.name} FAINTED`)
+        bot.sendMessage(chatId, `${itemDict[j]}\n${player.name} <b>FAINTED!</b>\n<pre>Tap Restart.</pre>`, {parse_mode: "HTML"})
+    } else {
+        bot.sendMessage(chatId, `${itemDict[j]}\nYour HP is ${player.life}.`, {parse_mode: "HTML"})
+    }
+}
+
+const itemFunc = (player) => {
+    player.item -= 1
+    if (player.life === undefined) {
+        player.life = 10
+    }
+    let j = IntRand(0, 6)
+    msgItem(player, j)
+
+}
+
+const main = ({token}) => {
     const bot = new TelegramBot(token, {polling: true})
     const Player = {}
     let player1 = Object.create(Player)
     let player2 = Object.create(Player)
     //  MAIN FUNCTION - START
     bot.on("message", (msg) => {
+        console.log(JSON.stringify(msg))
         let chatId = msg.chat.id
-        const mainKeyboard = {
-            reply_markup: {
-                keyboard: [
-                    ["Attack", "Defend"],
-                    ["Use an Item", "Roll a Dice"],
-                    ["Restart"],
-                ]
-            }, parse_mode: "HTML",
-        }
+        const {text} = msg
         // Functions Declaration - START //
-        const IntRand = (x, y) => {
-            return math.randomInt(x, y)
-        }
-        const checkPlayer = () => {
-            if (player1.id === undefined) {
-                player1.id = msg.from.id
-                player1.name = msg.from.first_name
-                console.log(`Player 1 = ${player1.name} , ID = ${player1.id}`)
-            } else if (player2.id === undefined && player1.id !== msg.from.id) {
-                player2.id = msg.from.id
-                player2.name = msg.from.first_name
-                console.log(`Player 2 = ${player2.name} , ID = ${player2.id}`)
-            } else if ((msg.from.id !== player1.id) && (msg.from.id !== player2.id)) {
-                bot.sendMessage(chatId, "There are already two players in the game. Please, wait until the current match is over!")
-            }
-        }
-        const rollDice = () => {
-            checkPlayer(player1.id, player1.name, player2.id, player2.name)
-            if (player1.id === msg.from.id) {
-                bot.sendMessage(chatId, `Player 1 = ${player1.name} , <pre>You got: ${IntRand(1, 21)} </pre>`, {parse_mode: "HTML"})
-            } else if (player2.id === msg.from.id) {
-                bot.sendMessage(chatId, `Player 2 = ${player2.name} , <pre>You got: ${IntRand(1, 21)} </pre>`, {parse_mode: "HTML"})
-            }
-        }
-        const restartGame = () => {
-            player1.life = 10
-            player1.id = undefined
-            player1.item = 1
-            player2.life = 10
-            player2.id = undefined
-            player2.item = 1
-            bot.sendMessage(chatId, "Game restarted! Please ROLL A DICE.")
-        }
-        const writeRank = () => {
-            if (player1.life <= 0) {
-                fs.writeFile("rank.txt", `${player2.name}\n`, {flag: "a"}, function (err) {
-                    if (err) throw err
-                })
-            } else if (player2.life <= 0) {
-                fs.writeFile("rank.txt", `${player1.name}\n`, {flag: "a"}, function (err) {
-                    if (err) throw err
-                })
-            }
-        }
         //Attack Function
-        const randAttack = () => {
-            fs.readFile("fileId.txt", function (err, data) {
-                if (err) throw err
-                let lines = data.toString().split("\n")
-                let randLine = lines[math.floor(math.random() * lines.length)]
-                bot.sendDocument(chatId, randLine)
-            })
-        }
-
         //Def Function
-
-        const msgDef = (player, x) => {
-            if (x === 5) {
-                player.dano = 1
-            }
-            else if (x === 7) {
-                player.dano = 0
-            }
-            else {
-                player.dano = x
-            }
-            player.life = player.life - player.dano
-            if (player.life <= 0) {
-                console.log(`${player.name} FAINTED`)
-                bot.sendMessage(chatId, `${player.name} <b>FAINTED!</b>\n<pre>Tap Restart.</pre>`, {parse_mode: "HTML"})
-            } else {
-                bot.sendMessage(chatId, ` ${dictDano[x]}\n${player.name} has ${player.life} HP!`, {parse_mode: "HTML"})
-            }
-        }
-
-        const defFunc = (player) => {
-            // TODO: FIX THIS!!
-            const n = 0
-            IntRand(0, 37, arrayDano[n])
-            let x = parseInt(arrayDano[n])
-            if (player.life === undefined) {
-                player.life = 10
-            }
-            console.log(`x = ${x}`)
-            msgDef(player, x)
-        }
-
         //Item Function
-        const msgItem = (player, j) => {
-            if (j === 4) {
-                player.life -= 3
-            }
-            else if (j === 0 || j === 5) {
-                player.life += 0
-            } else {
-                player.life += j
-            }
-            if (player.life <= 0) {
-                console.log(`${player.name} FAINTED`)
-                bot.sendMessage(chatId, `${itemDict[j]}\n${player.name} <b>FAINTED!</b>\n<pre>Tap Restart.</pre>`, {parse_mode: "HTML"})
-            } else {
-                bot.sendMessage(chatId, `${itemDict[j]}\nYour HP is ${player.life}.`, {parse_mode: "HTML"})
-            }
-        }
-        const itemFunc = (player) => {
-            player.item -= 1
-            if (player.life === undefined) {
-                player.life = 10
-            }
-            let j = IntRand(0, 6)
-            msgItem(player, j)
-
-        }
         // Functions Declaration END //
 
         //Damage array and Dict Damage
-        let arrayDano = ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "3", "3", "3", "4", "4", "6", "0", "0", "0", "0", "0", "0", "0", "0", "5", "5", "7", "7"]
-        let dictDano = {
-            0: ["Your opponent missed the attack!"],
-            1: ["It was effective! You lose 1 HP."],
-            2: ["It was effective! <b>CRITICAL HIT!</b> You lose 2 HP."],
-            3: ["It was ultra effective! You lose 3 HP."],
-            4: ["It was super effective! <b>CRITICAL HIT!</b> You lose 4 HP."],
-            5: ["It was effective! You lose 1 HP. You flinched! Opponent attacks again."],
-            6: ["It was ultra effective! <b>CRITICAL HIT!</b> You lose 6 HP."],
-            7: ["COUNTER! You take no damage and have an extra attack!"],
-        }
-        //Dict item
-        let itemDict = {
-            0: ["You found nothing!"],
-            1: ["You found a Potion and recovered 1 HP!"],
-            2: ["You found a Super Potion and recovered 2 HP!"],
-            3: ["You found a Hyper Potion and recovered 3 HP!"],
-            4: ["You found a Flame Orb and lost 3 HP."],
-            5: ["You found a Quick Claw and can attack twice!"],
-        }
+
         //Welcome msg and Menu
         const welcome = "/start"
         if (msg.text.indexOf(welcome) === 0) {
@@ -171,13 +186,13 @@ exports.main = ({token}) => {
         let adminId = 318475027
         if (msg.text.indexOf(restart) === 0) {
             if (msg.from.id === adminId) {
-                restartGame()
+                restartGame(msg, player1, player2, bot)
             } else if ((player1.life >= 1) && (player2.life >= 1) && !(player1.id === undefined)) {
                 bot.sendMessage(chatId, `${msg.from.first_name}, you cannot Restart a game in progress!`)
             } else if ((player1.id === undefined) || (player2.id === undefined)) {
                 bot.sendMessage(chatId, `${msg.from.first_name}, there is no match happening right now. Please roll a Dice to start!`)
             } else {
-                restartGame()
+                restartGame(msg, player1, player2, bot)
             }
         }
         //Attack
@@ -186,7 +201,7 @@ exports.main = ({token}) => {
             if ((player1.id === undefined) || (player2.id === undefined)) {
                 bot.sendMessage(chatId, `${msg.from.first_name}, you cannot play alone. Please, call a friend to join the game!`)
             } else {
-                checkPlayer(player1.id, player1.name, player2.id, player2.name)
+                checkPlayer(msg, player1, player2, bot)
                 if ((msg.from.id === player1.id) || (msg.from.id === player2.id)) {
                     randAttack()
                 }
@@ -237,5 +252,6 @@ exports.main = ({token}) => {
             }
         }
     })
-    //MAIN FUNCTION - END
 }
+
+exports = {main}
--- pokerand.test.js
+++ pokerand.test.js
@@ -1,5 +1,8 @@
 const sum = (a, b) => ({a, b})
 
+const mock_msg = {"message_id":7205,"from":{"id":116632598,"is_bot":false,"first_name":"Enzzo","username":"souenzzo","language_code":"en"},"chat":{"id":-209063185,"title":"RandPokeBot - Project Lab","type":"group","all_members_are_administrators":true},"date":1516237642,"reply_to_message":{"message_id":7164,"from":{"id":458733904,"is_bot":true,"first_name":"PokeRand Games","username":"Bertinnnbot"},"chat":{"id":-209063185,"title":"RandPokeBot - Project Lab","type":"group","all_members_are_administrators":true},"date":1516237493,"text":"Hello, Enzzo!\nWelcome to PokeRand Game\nTap Roll a Dice to start","entities":[{"offset":6,"length":7,"type":"bold"}]},"text":"Attack"}
+
+
 test("fact",
     () => {
         expect(sum(1, 2)).toEqual({a: 1, b: 2})
