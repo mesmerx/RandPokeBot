2eed01d4bc74a8194b54786e8745435719c87d0a
Luskinha <ldepaulaf@gmail.com>
2021-07-13 12:07:07 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const {main} = require('./pokerand')
 
 // const url = "https://api.telegram.org/bot"
-const token = '458733904:AAH-Fq8ABp5xVpLHf32uxKAbP-nMCLf4mgU'
+const token = ''
 
 
 main({token: token})
